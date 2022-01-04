dc907a1c07afc53cf66200c8c6229b1d3bbf113a
IsacHolstad <wisac22@gmail.com>
2022-01-04 14:35:49 +0100

    done trying

--- trying.js
+++ trying.js
@@ -1,4 +1,26 @@
 console.log("hello world");
 console.log(4 + 9);
+
 var pet = "cat";
+
 console.pet="cat";
+
+var FirstAnimale = "ra";
+var SecondAnimale = "bit";
+
+var SumOfMyAnimals = FirstAnimale + SecondAnimale;
+console.log(SumOfMyAnimals);
+
+var Firstvar = "3";
+var Secondvar = "3";
+
+var SumOfMyFirstvar
+
+var LightOn = "on";
+var LightOff = "off";
+
+var SumOfMyLights = LightOn + LightOff;
+console.log(SumOfMyLights);
+
+var isac = "12";
+ console.log(isac);
